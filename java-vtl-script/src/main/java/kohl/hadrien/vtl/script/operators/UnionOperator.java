83649500b3d810a85cd2a637e0a08c7daf0da72f
Hadrien Kohl <hadrien.kohl@gmail.com>
2016-10-24 13:30:47 +0200

    Implement the union operation and tests

--- java-vtl-script/src/main/java/kohl/hadrien/vtl/script/operators/UnionOperator.java
+++ java-vtl-script/src/main/java/kohl/hadrien/vtl/script/operators/UnionOperator.java
@@ -1,16 +1,114 @@
 package kohl.hadrien.vtl.script.operators;
 
-import kohl.hadrien.Dataset;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import kohl.hadrien.*;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Arrays.asList;
 
 /**
  * Union operator
  */
 public class UnionOperator implements Supplier<Dataset> {
 
+    private final DataStructure dataStructure;
+    private final List<Dataset> datasets;
+    private Set<Map.Entry<String, Class<? extends Component>>> requiredRoles;
+
+    public UnionOperator(Dataset... dataset) {
+        this(asList(dataset));
+    }
+
+    public UnionOperator(List<Dataset> datasets) {
+        checkArgument(
+                !checkNotNull(datasets, "the dataset list was null").isEmpty(),
+                "the dataset list was empty"
+        );
+        this.datasets = datasets;
+
+        Iterator<Dataset> iterator = this.datasets.iterator();
+        dataStructure = iterator.next().getDataStructure();
+        while (iterator.hasNext())
+            checkDataStructures(iterator.next());
+    }
+
+    private void checkDataStructures(Dataset dataset) {
+        // Identifiers and attribute should be equals in name, role and type.
+        Set<String> requiredNames = nonAttributeNames(this.dataStructure);
+        Set<String> providedNames = dataset.getDataStructure().names();
+
+        checkArgument(
+                requiredNames.equals(providedNames),
+                "dataset %s was incompatible with the required data structure, missing: %s, unexpected %s",
+                Sets.difference(requiredNames, providedNames),
+                Sets.difference(providedNames, requiredNames)
+        );
+
+        Map<String, Class<? extends Component>> requiredRoles;
+        requiredRoles = Maps.filterKeys(this.dataStructure.roles(), requiredNames::contains);
+        Map<String, Class<? extends Component>> providedRoles;
+        providedRoles = Maps.filterKeys(dataset.getDataStructure().roles(), requiredNames::contains);
+
+        checkArgument(
+                requiredNames.equals(providedNames),
+                "dataset %s was incompatible with the required data structure, missing: %s, unexpected %s",
+                Sets.difference(requiredRoles.entrySet(), providedRoles.entrySet()),
+                Sets.difference(providedRoles.entrySet(), requiredRoles.entrySet())
+        );
+
+        Map<String, Class<?>> requiredTypes;
+        requiredTypes = Maps.filterKeys(this.dataStructure.types(), requiredNames::contains);
+        Map<String, Class<?>> providedTypes;
+        providedTypes = Maps.filterKeys(dataset.getDataStructure().types(), requiredNames::contains);
+
+        checkArgument(
+                requiredNames.equals(providedNames),
+                "dataset %s was incompatible with the required data structure, missing: %s, unexpected %s",
+                Sets.difference(requiredTypes.entrySet(), providedTypes.entrySet()),
+                Sets.difference(providedTypes.entrySet(), requiredTypes.entrySet())
+        );
+
+    }
+
+    private Set<String> nonAttributeNames(DataStructure dataStructure) {
+        return Maps.filterValues(dataStructure.roles(), component -> !Attribute.class.equals(component)).keySet();
+    }
+
     @Override
     public Dataset get() {
-        return null;
+        if (datasets.size() == 1)
+            return datasets.get(0);
+
+        return new Dataset() {
+            @Override
+            public Set<List<Identifier>> cartesian() {
+                return null;
+            }
+
+            @Override
+            public DataStructure getDataStructure() {
+                return dataStructure;
+            }
+
+            @Override
+            public Stream<Tuple> get() {
+                // TODO: Attribute propagation.
+                ConcurrentHashMap.KeySetView<Tuple, Boolean> seen = ConcurrentHashMap.newKeySet();
+                return datasets.stream().flatMap(Supplier::get)
+                        .filter(seen::contains);
+
+            }
+        };
     }
+
 }
--- java-vtl-script/src/test/java/kohl/hadrien/vtl/script/operators/UnionOperatorTest.java
+++ java-vtl-script/src/test/java/kohl/hadrien/vtl/script/operators/UnionOperatorTest.java
@@ -1,9 +1,12 @@
 package kohl.hadrien.vtl.script.operators;
 
-import kohl.hadrien.Dataset;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kohl.hadrien.*;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -12,6 +15,7 @@ import static org.mockito.Mockito.when;
 
 public class UnionOperatorTest {
 
+    ObjectMapper mapper = new ObjectMapper();
 
     @Test
     public void testOneDatasetReturnedUnchanged() throws Exception {
@@ -21,12 +25,11 @@ public class UnionOperatorTest {
 
         UnionOperator operator = new UnionOperator(dataset);
 
-        assertThat(operator.get())
+        assertThat(operator.get().get())
                 .as("Check that result of union operation", null)
                 .isSameAs(dataset.get());
 
     }
-
 
 
     @Test
@@ -35,32 +38,120 @@ public class UnionOperatorTest {
         SoftAssertions softly = new SoftAssertions();
         try {
 
-            Dataset dataset1 = null;
-            Dataset dataset2 = null;
-            Dataset dataset3 = null;
+            DataStructure dataStructure = DataStructure.of(mapper::convertValue,
+                    "TIME", Identifier.class, String.class,
+                    "GEO", Identifier.class, String.class,
+                    "POP", Measure.class, Integer.class
+            );
+
+            Dataset dataset1 = mock(Dataset.class);
+            Dataset dataset2 = mock(Dataset.class);
+            Dataset dataset3 = mock(Dataset.class);
+
+            when(dataset1.getDataStructure()).thenReturn(dataStructure);
+            when(dataset2.getDataStructure()).thenReturn(dataStructure);
+            when(dataset3.getDataStructure()).thenReturn(dataStructure);
 
             UnionOperator unionOperator = new UnionOperator(dataset1, dataset2, dataset3);
             softly.assertThat(unionOperator).isNotNull();
 
-            Dataset wrongId = null;
+            DataStructure wrongStructure = DataStructure.of(mapper::convertValue,
+                    "TIME2", Identifier.class, String.class,
+                    "GEO2", Identifier.class, String.class,
+                    "POP2", Measure.class, Integer.class
+            );
+
+            Dataset wrongDataset = mock(Dataset.class);
+            when(wrongDataset.getDataStructure()).thenReturn(wrongStructure);
             Throwable expextedEx = null;
             try {
-                new UnionOperator(dataset1, wrongId, dataset2, dataset3);
+                new UnionOperator(dataset1, wrongDataset, dataset2, dataset3);
             } catch (Throwable t) {
                 expextedEx = t;
             }
-            assertThat(expextedEx).isNotNull();
-
-            Dataset wrongMeasure = null;
-            try {
-                new UnionOperator(dataset1, wrongMeasure, dataset2, dataset3);
-            } catch (Throwable t) {
-                expextedEx = t;
-            }
-            assertThat(expextedEx).isNotNull();
+            softly.assertThat(expextedEx).isNotNull().hasMessageContaining("incompatible");
 
         } finally {
             softly.assertAll();
         }
+    }
+
+    @Test
+    public void testUnion() throws Exception {
+
+        DataStructure dataStructure = DataStructure.of(mapper::convertValue,
+                "TIME", Identifier.class, String.class,
+                "GEO", Identifier.class, String.class,
+                "POP", Measure.class, Integer.class
+        );
+
+        Dataset totalPopulation1 = mock(Dataset.class);
+        Dataset totalPopulation2 = mock(Dataset.class);
+        when(totalPopulation1.getDataStructure()).thenReturn(dataStructure);
+        when(totalPopulation2.getDataStructure()).thenReturn(dataStructure);
+
+        when(totalPopulation1.get()).thenReturn(Stream.of(
+                tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Belgium"),
+                        dataStructure.wrap("TIME", 5)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Greece"),
+                        dataStructure.wrap("TIME", 2)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "France"),
+                        dataStructure.wrap("TIME", 3)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Malta"),
+                        dataStructure.wrap("TIME", 7)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Finland"),
+                        dataStructure.wrap("TIME", 9)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Switzerland"),
+                        dataStructure.wrap("TIME", 12)
+                )
+        ));
+
+        when(totalPopulation1.get()).thenReturn(Stream.of(
+                tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Netherlands"),
+                        dataStructure.wrap("TIME", 23)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Greece"),
+                        dataStructure.wrap("TIME", 2)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Spain"),
+                        dataStructure.wrap("TIME", 5)
+                ), tuple(
+                        dataStructure.wrap("TIME", "2012"),
+                        dataStructure.wrap("GEO", "Iceland"),
+                        dataStructure.wrap("TIME", 1)
+                )
+        ));
+
+        UnionOperator unionOperator = new UnionOperator(totalPopulation1, totalPopulation2);
+
+        assertThat(unionOperator.get()).isNotNull();
+        assertThat(unionOperator.get().stream()).isNotNull();
+
+    }
+
+
+    Dataset.Tuple tuple(Component... components) {
+        return new Dataset.AbstractTuple() {
+            @Override
+            protected List<Component> delegate() {
+                return Arrays.asList(components);
+            }
+        };
     }
 }
