eba50c00e9d2460e9dee48c13a27ef9467c742b6
Hadrien Kohl <hadrien.kohl@gmail.com>
2016-11-01 22:30:00 +0100

    Add OecdConnector class and test

--- java-vtl-connector/oecd/src/main/java/hadrien/kohl/vtl/connector/oecd/OecdConnector.java
+++ java-vtl-connector/oecd/src/main/java/hadrien/kohl/vtl/connector/oecd/OecdConnector.java
@@ -3,11 +3,21 @@ package hadrien.kohl.vtl.connector.oecd;
 import kohl.hadrien.vtl.connector.Connector;
 import kohl.hadrien.vtl.connector.ConnectorException;
 import kohl.hadrien.vtl.model.Dataset;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * A {@link Connector} implementation to access data from OECD.
  */
 public class OecdConnector implements Connector {
+
+    // Search URL:
+    static final String SEARCH = "https://data.oecd.org/search-api/";
+    private final RestTemplate restTemplate;
+
+    public OecdConnector() {
+        this.restTemplate = new RestTemplate();
+    }
+
     @Override
     public boolean canHandle(String identifier) {
         throw new UnsupportedOperationException("Not implemented yet");
---
+++ java-vtl-connector/oecd/src/test/java/hadrien/kohl/vtl/connector/oecd/OecdConnectorTest.java
@@ -0,0 +1,7 @@
+package hadrien.kohl.vtl.connector.oecd;
+
+import static org.junit.Assert.*;
+
+public class OecdConnectorTest {
+    // http://stats.oecd.org/SDMX-JSON/data/FISH_FLD/PGRT.USD.BEL+CHL+DNK+EST+FIN+FRA+DEU+GRC+ISL+IRL+LVA+MEX+NLD+NOR+POL+PRT+ESP+SWE+GBR+USA+NMEC+COL+THA/all?startTime=2000&endTime=2014
+}
