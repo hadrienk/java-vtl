e5e11b40d35e94acd39b0f95991a90658f3c0a35
Pawel Buczek <pawel.buczek@ssb.no>
2017-02-06 09:56:55 +0100

    KP-2055 Adds getDataStructure() and part of get() implementation

--- java-vtl-script/src/main/java/no/ssb/vtl/script/operations/CheckOperation.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/operations/CheckOperation.java
@@ -2,14 +2,22 @@ package no.ssb.vtl.script.operations;
 
 import com.google.common.collect.Maps;
 import no.ssb.vtl.model.Component;
+import no.ssb.vtl.model.DataPoint;
+import no.ssb.vtl.model.DataStructure;
 import no.ssb.vtl.model.Dataset;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.*;
 
-public class CheckOperation {
+public class CheckOperation implements Dataset{
 
     private static final List<String> ROWS_TO_RETURN_POSSIBLE_VALUES = Arrays.asList("not_valid", "valid", "all");
     private static final List<String> COMPONENTS_TO_RETURN_POSSIBLE_VALUES = Arrays.asList("measures", "condition");
@@ -19,10 +27,10 @@ public class CheckOperation {
     private final String componentsToReturn;
     private final String errorCode;
     private final Integer errorLevel;
+    private DataStructure cache;
 
     public CheckOperation(Dataset dataset, String rowsToReturn, String componentsToReturn, String errorCode, Integer errorLevel) {
         this.dataset = checkNotNull(dataset, "dataset was null");
-        checkDataStructure(this.dataset);
 
         if (rowsToReturn != null) {
             checkArgument(!rowsToReturn.isEmpty(), "the rowsToReturn argument was empty");
@@ -44,6 +52,10 @@ public class CheckOperation {
             this.componentsToReturn = "measures";
         }
 
+        checkArgument(!("measures".equals(componentsToReturn) && "all".equals(rowsToReturn)), "cannot use 'all' with 'measures' parameter");
+
+        checkDataStructure(this.dataset);
+
         if (errorCode != null) {
             checkArgument(!errorCode.isEmpty(), "the errorCode argument was empty");
             this.errorCode = errorCode;
@@ -62,5 +74,94 @@ public class CheckOperation {
         long noBooleanMeasures = dataset.getDataStructure().values().stream().filter(c -> c.isMeasure() && c.getType().equals(Boolean.class)).count();
         checkArgument(noBooleanMeasures < 2, "dataset has too many boolean measure components");
         checkArgument(noBooleanMeasures > 0, "dataset has no boolean measure component");
+    }
+
+    @Override
+    public DataStructure getDataStructure() {
+        if (cache == null) {
+            Map<String, Component.Role> newRoles = new HashMap<>(dataset.getDataStructure().getRoles());
+            Map<String, Class<?>> newTypes = new HashMap<>(dataset.getDataStructure().getTypes());
+            Set<String> oldNames = dataset.getDataStructure().keySet();
+
+            if (componentsToReturn == null || "measures".equals(componentsToReturn)) {
+                removeAllComponentsButIdentifiersAndMeasures(newRoles, newTypes, oldNames);
+            } else if ("condition".equals(componentsToReturn)) {
+                removeAllComponentsButIdentifiers(newRoles, newTypes, oldNames);
+                addComponent("CONDITION", newRoles, newTypes, Component.Role.MEASURE, Boolean.class);
+            }
+
+            addComponent("errorcode", newRoles, newTypes, Component.Role.ATTRIBUTE, String.class);
+            addComponent("errorlevel", newRoles, newTypes, Component.Role.ATTRIBUTE, Integer.class);
+
+            BiFunction<Object, Class<?>, ?> converter = dataset.getDataStructure().converter();
+            cache = DataStructure.of(converter, newTypes, newRoles);
+        }
+
+        return cache;
+    }
+
+    private void addComponent(String componentName, Map<String, Component.Role> newRoles,
+                              Map<String, Class<?>> newTypes, Component.Role role, Class<?> aClass) {
+        newRoles.put(componentName, role);
+        newTypes.put(componentName, aClass);
+    }
+
+    private void removeAllComponentsButIdentifiers(Map<String, Component.Role> newRoles,
+                                                   Map<String, Class<?>> newTypes, Set<String> oldNames) {
+        for (String oldName : oldNames) {
+            if (newRoles.get(oldName) != Component.Role.IDENTIFIER) {
+                newRoles.remove(oldName);
+                newTypes.remove(oldName);
+            }
+        }
+    }
+
+    private void removeAllComponentsButIdentifiersAndMeasures(Map<String, Component.Role> newRoles,
+                                                   Map<String, Class<?>> newTypes, Set<String> oldNames) {
+        for (String oldName : oldNames) {
+            if (newRoles.get(oldName) != Component.Role.IDENTIFIER && newRoles.get(oldName) != Component.Role.MEASURE) {
+                newRoles.remove(oldName);
+                newTypes.remove(oldName);
+            }
+        }
+    }
+
+    @Override
+    public Stream<Tuple> get() {
+        Stream<Tuple> tupleStream = dataset.get();
+
+        DataPoint errorCodeDataPoint = getDataStructure().wrap("errorcode", errorCode);
+        DataPoint errorLevelDataPoint = getDataStructure().wrap("errorlevel", errorCode);
+
+        if (rowsToReturn == null || "not_valid".equals(rowsToReturn)) {
+            tupleStream = tupleStream.filter(tuple -> tuple.values().stream()
+                    .filter(dataPoint -> dataPoint.getComponent().isMeasure() && dataPoint.getType().equals(Boolean.class))
+                    .anyMatch(dataPoint -> dataPoint.get().equals(false))).peek(e -> System.out.println("value: " + e));
+        } else if ("valid".equals(rowsToReturn)) {
+            tupleStream = tupleStream.filter(tuple -> tuple.values().stream()
+                    .filter(dataPoint -> dataPoint.getComponent().isMeasure() && dataPoint.getType().equals(Boolean.class))
+                    .anyMatch(dataPoint -> dataPoint.get().equals(true))).peek(e -> System.out.println("value: " + e));
+        } //else if ("all".equals(rowsToReturn)) //all is not filtered
+
+        if (componentsToReturn == null || "measures".equals(componentsToReturn)) {
+            tupleStream = tupleStream.map(dataPoints -> {
+                        List<DataPoint> dataPointsNewList = new ArrayList<>(dataPoints);
+                        dataPointsNewList.add(errorCodeDataPoint);
+                        dataPointsNewList.add(errorLevelDataPoint);
+                        return Tuple.create(dataPointsNewList);
+                    });
+        } else if ("condition".equals(componentsToReturn)) {
+            //TODO here
+//            new DataPoint(getDataStructure().get("CONDITION")) {
+//                @Override
+//                public Object get() {
+//                    return dataPoints.values().stream()
+//                            .filter(dp -> dp.getRole() == Component.Role.MEASURE && dp.getType().equals(Boolean.class))
+//                            .findFirst().orElseThrow(() -> new IllegalArgumentException("DataPoint of type Boolean and role MEASURE not found in stream"));
+//                }
+//            });
+        }
+
+        return tupleStream;
     }
 }
--- java-vtl-script/src/test/java/no/ssb/vtl/script/operations/check/CheckOperationTest.java
+++ java-vtl-script/src/test/java/no/ssb/vtl/script/operations/check/CheckOperationTest.java
@@ -1,6 +1,7 @@
 package no.ssb.vtl.script.operations.check;
 
 import no.ssb.vtl.model.Component;
+import no.ssb.vtl.model.DataPoint;
 import no.ssb.vtl.model.DataStructure;
 import no.ssb.vtl.model.Dataset;
 import no.ssb.vtl.script.operations.CheckOperation;
@@ -8,6 +9,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 
@@ -38,6 +45,18 @@ public class CheckOperationTest {
     }
 
     /**
+     * NOTE: This is not in the spec for check with single rule, but exists for check with datapoint rulesets.
+     * <p>
+     * VTL 1.1, line 4799.
+     */
+    @Test
+    public void testArgumentAllAndMeasuresToReturn() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expect(hasProperty("message", containsString("cannot use 'all' with 'measures'")));
+        new CheckOperation(mock(Dataset.class), "all", "measures", null, null);
+    }
+
+    /**
      * The input Dataset must have all Boolean Measure Components.
      * NOTE: The spec is not consistent with regard to parameters and constraints sections (line 4925-4926 and 4949).
      * We decided to require one and only one Boolean Measure Component.
@@ -59,6 +78,76 @@ public class CheckOperationTest {
         thrown.expect(IllegalArgumentException.class);
         thrown.expect(hasProperty("message", containsString("too many boolean")));
         new CheckOperation(dataset, "valid", "measure", null, null);
+    }
+
+    @Test
+    public void testCheck() throws Exception {
+        DataStructure dataStructure = DataStructure.of((s, o) -> s,
+                "kommune_nr", Component.Role.IDENTIFIER, String.class,
+                "code", Component.Role.IDENTIFIER, String.class, //from KLASS
+                "CONDITION", Component.Role.MEASURE, Boolean.class
+        );
+
+        Dataset ds = mock(Dataset.class);
+        when(ds.getDataStructure()).thenReturn(dataStructure);
+
+        when(ds.get()).thenReturn(Stream.of(
+                tuple(
+                        dataStructure.wrap("kommune_nr", "0101"),
+                        dataStructure.wrap("code", "0101"),
+                        dataStructure.wrap("CONDITION", true)
+                ), tuple(
+                        dataStructure.wrap("kommune_nr", "9990"),
+                        dataStructure.wrap("code", null), //not in the code list, so a null value
+                        dataStructure.wrap("CONDITION", false)
+                ), tuple(
+                        dataStructure.wrap("kommune_nr", "0104"),
+                        dataStructure.wrap("code", "0104"),
+                        dataStructure.wrap("CONDITION", true)
+                )
+        ));
+
+        CheckOperation checkOperation = new CheckOperation(ds, "not_valid", "measures", null, null);
+
+        assertThat(checkOperation.getDataStructure().getRoles()).contains(
+                entry("kommune_nr", Component.Role.IDENTIFIER),
+                entry("code", Component.Role.IDENTIFIER),
+                entry("CONDITION", Component.Role.MEASURE),
+                entry("errorcode", Component.Role.ATTRIBUTE),
+                entry("errorlevel", Component.Role.ATTRIBUTE)
+        );
+
+        Stream<Dataset.Tuple> stream = checkOperation.stream();
+        assertThat(stream).isNotNull();
+
+        List<Dataset.Tuple> collect = stream.collect(toList());
+        assertThat(collect).
+                extractingResultOf("ids").isNotEmpty().hasSize(1);
+
+        assertThat(getDataPoint(collect, "kommune_nr").get()).isEqualTo("9990");
+        assertThat(getDataPoint(collect, "code").get()).isNull(); //not in the code list, so a null value
+        assertThat(getDataPoint(collect, "CONDITION").get()).isEqualTo("false");
+        assertThat(getDataPoint(collect, "errorlevel").get()).isNull();
+        assertThat(getDataPoint(collect, "errorcode").get()).isNull();
+    }
+
+    private DataPoint getDataPoint(List<Dataset.Tuple> tuple, String componentName) {
+        List<DataPoint> dpListOfOne = tuple.get(0).stream()
+                .filter(dp -> dp.getName().equals(componentName))
+                .collect(toList());
+
+        assertThat(dpListOfOne).hasSize(1);
+
+        return dpListOfOne.get(0);
+    }
+
+    private Dataset.Tuple tuple(DataPoint... components) {
+        return new Dataset.AbstractTuple() {
+            @Override
+            protected List<DataPoint> delegate() {
+                return Arrays.asList(components);
+            }
+        };
     }
 
 }
