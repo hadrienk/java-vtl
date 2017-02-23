9e7693ff3b262e63488f53b64b284f8815b66367
Hadrien Kohl <hadrien.kohl@gmail.com>
2017-02-23 10:12:31 +0100

    Add AbtractDatasetOperation and AbstractUnaryDatasetOperation classes
    FilterOperation is modified to use AbstractUnaryDatasetOperation.

--- java-vtl-script/src/main/java/no/ssb/vtl/script/operations/FilterOperator.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/operations/FilterOperator.java
@@ -1,6 +1,7 @@
 package no.ssb.vtl.script.operations;
 
 import com.google.common.base.MoreObjects;
+import no.ssb.vtl.model.AbstractUnaryDatasetOperation;
 import no.ssb.vtl.model.DataStructure;
 import no.ssb.vtl.model.Dataset;
 
@@ -10,37 +11,32 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
 
-public class FilterOperator implements Dataset{
-    
-    // The dataset we are applying the FilterOperator on.
-    private final Dataset dataset;
+public class FilterOperator extends AbstractUnaryDatasetOperation {
+
     private final Predicate<DataPoint> predicate;
-    
-    private DataStructure cache;
-    
+
     public FilterOperator(Dataset dataset, Predicate<DataPoint> predicate) {
-        this.dataset = checkNotNull(dataset, "the dataset was null");
+        super(checkNotNull(dataset, "the dataset was null"));
         this.predicate = checkNotNull(predicate, "the predicate was null");
     }
-    
-    @Override
-    public DataStructure getDataStructure() {
-        return cache = (cache == null ? dataset.getDataStructure() : cache);
+
+    protected DataStructure computeDataStructure() {
+        return getChild().getDataStructure();
     }
-    
+
     @Override
     public Stream<DataPoint> get() {
-        return this.dataset.filter(predicate).stream();
+        return getChild().filter(predicate).stream();
     }
-    
+
     @Override
     public String toString() {
         MoreObjects.ToStringHelper helper = MoreObjects.toStringHelper(this);
-        Map<Boolean, List<DataPoint>> predicateResultMap = dataset.stream().collect(Collectors.partitioningBy(predicate));
+        Map<Boolean, List<DataPoint>> predicateResultMap = getChild().stream().collect(Collectors.partitioningBy(predicate));
         helper.addValue(predicateResultMap);
-        helper.add("structure", cache);
+        helper.add("structure", getDataStructure());
         return helper.omitNullValues().toString();
     }
 }
