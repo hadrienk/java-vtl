75decc51499b27f899ac7d482d3a17bee12e0b48
Hadrien Kohl <hadrien.kohl@gmail.com>
2017-03-23 17:46:58 +0100

    Connect the visitors

--- java-vtl-script/src/main/java/no/ssb/vtl/script/visitors/HierarchyVisitor.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/visitors/HierarchyVisitor.java
@@ -1,7 +1,10 @@
 package no.ssb.vtl.script.visitors;
 
+import no.ssb.vtl.model.Component;
 import no.ssb.vtl.model.Dataset;
 import no.ssb.vtl.parser.VTLBaseVisitor;
+import no.ssb.vtl.parser.VTLParser;
+import no.ssb.vtl.script.operations.hierarchy.HierarchyOperation;
 
 import static com.google.common.base.Preconditions.*;
 
@@ -11,5 +14,17 @@ public class HierarchyVisitor extends VTLBaseVisitor<Dataset> {
 
     public HierarchyVisitor(ReferenceVisitor referenceVisitor) {
         this.referenceVisitor = checkNotNull(referenceVisitor);
+    }
+
+    @Override
+    public Dataset visitHierarchyExpression(VTLParser.HierarchyExpressionContext ctx) {
+
+        // Safe.
+        Dataset dataset = (Dataset) referenceVisitor.visit(ctx.datasetRef());
+        Dataset hierarchyDataset = (Dataset) referenceVisitor.visit(ctx.hierarchyReference().datasetRef());
+        Component component = (Component) referenceVisitor.visitComponentRef(ctx.componentRef());
+
+        return new HierarchyOperation(dataset, hierarchyDataset, component);
+
     }
 }
