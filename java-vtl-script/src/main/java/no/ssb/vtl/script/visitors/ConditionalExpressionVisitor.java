fb8d11ab20e112cbfc14e95b18c4c68fd4fcaf7f
Pawel Buczek <pawel.buczek@ssb.no>
2017-03-30 15:00:44 +0200

    KP-1413 Better logging

--- java-vtl-script/src/main/java/no/ssb/vtl/script/visitors/ConditionalExpressionVisitor.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/visitors/ConditionalExpressionVisitor.java
@@ -30,7 +30,8 @@ public class ConditionalExpressionVisitor extends VTLBaseVisitor<VTLExpression> 
         Object repValue = paramVisitor.visit(ctx.nvlRepValue);
 
         if (input.getType() != repValue.getClass()) {
-            throw new ParseCancellationException("The value to replace null must be of type " + input.getType());
+            throw new ParseCancellationException("The value to replace null must be of type " + input.getType()
+            + ", but was: " + repValue.getClass() + ". Replacement value was: " + repValue);
         }
 
         return new VTLExpression.Builder(input.getType(), dataPoint -> {
