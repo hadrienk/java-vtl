78eaace6c0dd2eeab66dff3dfe6e12ba52714308
Hadrien Kohl <hadrien.kohl@gmail.com>
2017-05-03 14:28:48 +0200

    Invert the test since it should fail until we have a better type model

--- java-vtl-model/src/test/java/no/ssb/vtl/model/VTLObjectTest.java
+++ java-vtl-model/src/test/java/no/ssb/vtl/model/VTLObjectTest.java
@@ -1,5 +1,6 @@
 package no.ssb.vtl.model;
 
+import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.junit.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -35,16 +36,38 @@ public class VTLObjectTest {
     }
 
     @Test
-    public void testNumberCompare() throws Exception {
+    public void testDifferentNumberCompare() throws Exception {
 
         VTLObject one = VTLObject.of(new Double(1));
         VTLObject two = VTLObject.of(new Integer(2));
         VTLObject otherOne = VTLObject.of(new Long(1));
 
-        assertThat(one.compareTo(two)).isLessThan(0);
-        assertThat(two.compareTo(one)).isGreaterThan(0);
-        assertThat(one.compareTo(otherOne)).isEqualTo(0);
-        assertThat(otherOne.compareTo(one)).isEqualTo(0);
+        try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+            softly.assertThatThrownBy(() -> one.compareTo(two))
+                    .hasMessageContaining(" 1.0 ")
+                    .hasMessageContaining("Double")
+                    .hasMessageContaining(" 2 ")
+                    .hasMessageContaining("Integer");
+
+            softly.assertThatThrownBy(() -> two.compareTo(one))
+                    .hasMessageContaining(" 2 ")
+                    .hasMessageContaining("Double")
+                    .hasMessageContaining(" 1.0 ")
+                    .hasMessageContaining("Integer");
+
+            softly.assertThatThrownBy(() -> assertThat(one.compareTo(otherOne)))
+                    .hasMessageContaining(" 1 ")
+                    .hasMessageContaining("Long")
+                    .hasMessageContaining(" 1.0 ")
+                    .hasMessageContaining("Double");
+
+
+            softly.assertThatThrownBy(() -> assertThat(otherOne.compareTo(one)))
+                    .hasMessageContaining(" 1 ")
+                    .hasMessageContaining("Long")
+                    .hasMessageContaining(" 1.0 ")
+                    .hasMessageContaining("Double");
+        }
 
 
     }
