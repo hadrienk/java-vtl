3450969a6bd463fd7340561ea09b2bd3108287a7
Høiseth-Gilje, Eivind <Eivind.Hoiseth-Gilje@ssb.no>
2017-08-18 14:47:58 +0200

    Module for parsing a vtl expression to find the dependencies of an assignment

---
+++ java-vtl-dependency-parser/src/main/java/no/ssb/vtl/dependencies/AssignmentListener.java
@@ -0,0 +1,62 @@
+package no.ssb.vtl.dependencies;
+
+/*-
+ * ========================LICENSE_START=================================
+ * Java VTL
+ * %%
+ * Copyright (C) 2016 - 2017 Hadrien Kohl
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+
+import com.google.common.collect.ImmutableSet;
+import no.ssb.vtl.parser.VTLBaseListener;
+import no.ssb.vtl.parser.VTLParser;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class AssignmentListener extends VTLBaseListener {
+    
+    private Map<String, Set<ComponentRef>> variableDependency;
+    
+    private Set<ComponentRef> componentRefs = new HashSet<>();
+    
+    public AssignmentListener() {
+        variableDependency = new HashMap<>();
+    }
+    
+    @Override
+    public void exitJoinCalcClause(VTLParser.JoinCalcClauseContext ctx) {
+        String identifier = ctx.identifier().getText();
+        variableDependency.put(identifier, ImmutableSet.copyOf(componentRefs));
+        componentRefs.clear();
+    }
+    
+    @Override
+    public void exitComponentRef(VTLParser.ComponentRefContext ctx) {
+    
+        String datasetRef = ctx.datasetRef().getText();
+        String variableRef = ctx.variableRef().getText();
+        componentRefs.add(new ComponentRef(datasetRef, variableRef));
+    }
+    
+    public Map<String, Set<ComponentRef>> getVariableDependency() {
+        return variableDependency;
+    }
+    
+}
+
---
+++ java-vtl-dependency-parser/src/test/java/no/ssb/vtl/dependencies/DependencyParserTest.java
@@ -0,0 +1,53 @@
+package no.ssb.vtl.dependencies;
+
+/*-
+ * ========================LICENSE_START=================================
+ * Java VTL
+ * %%
+ * Copyright (C) 2016 - 2017 Hadrien Kohl
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class DependencyParserTest {
+    
+    private DependencyParser parser = new DependencyParser();
+    
+    @Rule
+    public final JUnitSoftAssertions softly = new JUnitSoftAssertions();
+    
+    @Test
+    public void parse() throws Exception {
+        String vtlExpression = "r := [t1, t2]{ var := t1.var1 / t2.var2, otherVar := t1.var3 * 1000}";
+        Map<String, Set<ComponentRef>> dependencies = parser.parse(vtlExpression);
+    
+        System.out.println(dependencies);
+        softly.assertThat(dependencies.get("var"))
+                .extracting("datasetId", "variableId")
+                .containsExactlyInAnyOrder(tuple("t1", "var1"), tuple("t2", "var2"));
+        softly.assertThat(dependencies.get("otherVar"))
+                .extracting("datasetId", "variableId")
+                .containsExactlyInAnyOrder(tuple("t1", "var3"));
+    
+    }
+}
