cba3f6f2b3a62c08ff95378e65269ca38a68f74d
Hadrien Kohl <hadrien.kohl@gmail.com>
2017-10-02 11:20:16 +0200

    Remove assignment loop

--- java-vtl-script/src/main/java/no/ssb/vtl/script/VTLScriptEngine.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/VTLScriptEngine.java
@@ -187,8 +187,8 @@ public class VTLScriptEngine extends AbstractScriptEngine {
             // Run loop.
             AssignmentVisitor assignmentVisitor = new AssignmentVisitor(context, connectors);
             Dataset last = null;
-            for (VTLParser.AssignmentContext assignmentContext : start.assignment()) {
-                last = assignmentVisitor.visit(assignmentContext);
+            for (VTLParser.StatementContext statementContext : start.statement()) {
+                last = assignmentVisitor.visit(statementContext);
             }
             return last;
 
