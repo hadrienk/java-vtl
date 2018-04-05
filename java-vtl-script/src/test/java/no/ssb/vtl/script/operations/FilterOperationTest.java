f57b4c8b14496d671a95dd5554218b775ae54517
Hadrien Kohl <hadrien.kohl@ssb.no>
2018-04-05 15:07:47 +0200

    Check that filter operation is closing the stream it uses

--- java-vtl-script/src/test/java/no/ssb/vtl/script/operations/FilterOperationTest.java
+++ java-vtl-script/src/test/java/no/ssb/vtl/script/operations/FilterOperationTest.java
@@ -9,9 +9,9 @@ package no.ssb.vtl.script.operations;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,23 +20,21 @@ package no.ssb.vtl.script.operations;
  * =========================LICENSE_END==================================
  */
 
-import no.ssb.vtl.model.Component;
-import no.ssb.vtl.model.Dataset;
-import no.ssb.vtl.model.StaticDataset;
-import no.ssb.vtl.model.VTLBoolean;
-import no.ssb.vtl.model.VTLExpression;
-import no.ssb.vtl.model.VTLObject;
+import no.ssb.vtl.model.*;
 import no.ssb.vtl.script.operations.join.ComponentBindings;
+import no.ssb.vtl.script.support.DatasetCloseWatcher;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.script.Bindings;
 
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilterOperationTest {
 
-    private Dataset dataset;
+    private DatasetCloseWatcher dataset;
     private ComponentBindings componentBindings;
 
     private static VTLExpression NULL = new VTLExpression() {
@@ -77,26 +75,33 @@ public class FilterOperationTest {
 
     @Before
     public void setUp() {
-        dataset = StaticDataset.create()
+        dataset = DatasetCloseWatcher.wrap(StaticDataset.create()
                 .addComponent("id", Component.Role.IDENTIFIER, String.class)
                 .addComponent("mc", Component.Role.MEASURE, String.class)
                 .addPoints("idValue", null)
-                .build();
+                .build());
         componentBindings = new ComponentBindings(dataset);
     }
 
     @Test
     public void testPredicateReturnsNull() {
-
         FilterOperation resultBooleanNull = new FilterOperation(dataset, NULL, componentBindings);
-        assertThat(resultBooleanNull.getData()).isEmpty();
+        try (Stream<DataPoint> data = resultBooleanNull.getData()) {
+            assertThat(data).isEmpty();
+        } finally {
+            assertThat(dataset.allStreamWereClosed()).isTrue();
+        }
     }
 
     @Test
     public void testPredicateReturnsFalse() {
 
         FilterOperation result = new FilterOperation(dataset, FALSE, componentBindings);
-        assertThat(result.getData()).isEmpty();
+        try (Stream<DataPoint> data = result.getData()) {
+            assertThat(data).isEmpty();
+        } finally {
+            assertThat(dataset.allStreamWereClosed()).isTrue();
+        }
     }
 
 
@@ -104,7 +109,11 @@ public class FilterOperationTest {
     public void testPredicateReturnsTrue() {
 
         FilterOperation result = new FilterOperation(dataset, TRUE, componentBindings);
-        assertThat(result.getData()).isNotEmpty();
+        try (Stream<DataPoint> data = result.getData()) {
+            assertThat(data).isNotEmpty();
+        } finally {
+            assertThat(dataset.allStreamWereClosed()).isTrue();
+        }
     }
 }
 
