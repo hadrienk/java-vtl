ee453d2144cb5a8594e883300bf677f4d7f75413
Hadrien Kohl <hadrien.kohl@gmail.com>
2018-04-26 19:30:30 +0200

    Reduce cognitive complexity in MinimumSelector

--- java-vtl-script/src/main/java/no/ssb/vtl/script/operations/union/MinimumSelector.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/operations/union/MinimumSelector.java
@@ -9,9 +9,9 @@ package no.ssb.vtl.script.operations.union;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,18 +33,20 @@ class MinimumSelector<T> implements Selector<T> {
     }
 
     @Override
-    public Integer apply(T[] dataPoints) {
-        // Find the lowest value
-        T minVal = null;
-        int idx = -1;
-        for (int i = 0; i < dataPoints.length; i++) {
-            T dataPoint = dataPoints[i];
-            if (dataPoint == null)
-                continue;
+    public Integer apply(T[] values) {
 
-            if (minVal == null || comparator.compare(dataPoint, minVal) < 0) {
-                minVal = dataPoint;
+        // Find the lowest value
+        T minimum = null;
+        int idx = -1;
+        for (int i = 0; i < values.length; i++) {
+            T current = values[i];
+
+            if (current == null)
+                continue;
+            
+            if (minimum == null || comparator.compare(current, minimum) < 0) {
                 idx = i;
+                minimum = current;
             }
         }
         return idx;
