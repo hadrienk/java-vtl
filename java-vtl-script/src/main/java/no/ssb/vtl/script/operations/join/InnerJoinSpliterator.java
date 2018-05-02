662a89cf11894a471bf3829cedbda2d3e29b1bb3
Hadrien Kohl <hadrien.kohl@gmail.com>
2018-05-02 17:36:49 +0200

    Make sure InnerJoinSpliterator respect the Spliterator contract
    Fixes #76

--- java-vtl-script/src/main/java/no/ssb/vtl/script/operations/join/InnerJoinSpliterator.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/operations/join/InnerJoinSpliterator.java
@@ -1,12 +1,17 @@
 package no.ssb.vtl.script.operations.join;
 
+import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
 import com.google.common.collect.PeekingIterator;
 import com.google.common.collect.Queues;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
@@ -22,11 +27,13 @@ public class InnerJoinSpliterator<I, O> implements Spliterator<O> {
     private final PeekingIterator<I> leftIterator;
     private final PeekingIterator<I> rightIterator;
 
+    private Iterator<O> output = Collections.emptyIterator();
+
     public InnerJoinSpliterator(Comparator<I> predicate, BiFunction<I, I, O> merger, Spliterator<I> leftSpliterator, Spliterator<I> rightSpliterator) {
         this.predicate = checkNotNull(predicate);
         this.merger = checkNotNull(merger);
-        this.leftIterator = Iterators.peekingIterator(new SpliteratorIterator<>(leftSpliterator));
-        this.rightIterator = Iterators.peekingIterator(new SpliteratorIterator<>(rightSpliterator));
+        this.leftIterator = Iterators.peekingIterator(Spliterators.iterator(leftSpliterator));
+        this.rightIterator = Iterators.peekingIterator(Spliterators.iterator(rightSpliterator));
     }
 
     private void advance(PeekingIterator<I> source, Deque<I> buffer) {
@@ -49,34 +56,64 @@ public class InnerJoinSpliterator<I, O> implements Spliterator<O> {
     }
 
     @Override
-    public boolean tryAdvance(Consumer<? super O> action) {
+    public void forEachRemaining(Consumer<? super O> action) {
         advanceLeft();
         advanceRight();
-
         while (!leftBuffer.isEmpty() && !rightBuffer.isEmpty()) {
-            int compare = predicate.compare(
-                    leftBuffer.getFirst(),
-                    rightBuffer.getFirst()
-            );
-            if (0 < compare) {
-                // left > right (right is behind)
-                advanceRight();
-            } else if (compare < 0) {
-                // left < right (left is behind)
-                advanceLeft();
-            } else {
+            if (compare()) {
                 // output hit
-                break;
+                flushBuffers(action);
+                advanceLeft();
+                advanceRight();
             }
         }
+    }
 
+    private boolean compare() {
+        int compare = predicate.compare(
+                leftBuffer.getFirst(),
+                rightBuffer.getFirst()
+        );
+        if (0 < compare) {
+            // left > right (right is behind)
+            advanceRight();
+        } else if (compare < 0) {
+            // left < right (left is behind)
+            advanceLeft();
+        }
+        return compare == 0;
+    }
+
+    private void flushBuffers(Consumer<? super O> action) {
         for (I left : leftBuffer) {
             for (I right : rightBuffer) {
                 action.accept(merger.apply(left, right));
             }
         }
+    }
 
-        return !leftBuffer.isEmpty() || !rightBuffer.isEmpty();
+    @Override
+    public boolean tryAdvance(Consumer<? super O> action) {
+        if (output.hasNext()) {
+            action.accept(output.next());
+            return true;
+        }
+
+        advanceLeft();
+        advanceRight();
+
+        while (!leftBuffer.isEmpty() && !rightBuffer.isEmpty()) {
+            if (compare()) {
+                // output hit
+                output = Iterators.transform(
+                        new CartesianIterator<>(leftBuffer, rightBuffer),
+                        input -> merger.apply(input.get(0), input.get(1))
+                );
+                return tryAdvance(action);
+            }
+        }
+
+        return !leftBuffer.isEmpty() && !rightBuffer.isEmpty();
     }
 
     @Override
@@ -86,11 +123,11 @@ public class InnerJoinSpliterator<I, O> implements Spliterator<O> {
 
     @Override
     public long estimateSize() {
-        return 0;
+        return Long.MAX_VALUE;
     }
 
     @Override
     public int characteristics() {
-        return 0;
+        return Spliterator.SORTED & Spliterator.ORDERED;
     }
 }
