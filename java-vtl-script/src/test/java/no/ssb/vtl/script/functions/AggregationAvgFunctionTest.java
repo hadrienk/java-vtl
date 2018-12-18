682e54a64be4a5053d08a76ec306f02c9611cd7a
Hadrien Kohl <hadrien.kohl@gmail.com>
2018-12-18 14:07:39 +0100

    Test return value type of the aggregation functions

--- java-vtl-script/src/test/java/no/ssb/vtl/script/functions/AggregationAvgFunctionTest.java
+++ java-vtl-script/src/test/java/no/ssb/vtl/script/functions/AggregationAvgFunctionTest.java
@@ -12,9 +12,9 @@ package no.ssb.vtl.script.functions;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AggregationAvgFunctionTest {
 
@@ -82,7 +82,7 @@ public class AggregationAvgFunctionTest {
                 VTLNumber.of(number1),
                 VTLNumber.of(number2),
                 null,
-                VTLNumber.of((Double)null)
+                VTLNumber.of((Double) null)
         );
         VTLNumber<?> result = new AggregationAvgFunction().apply(numbers);
         assertThat(result).isEqualTo(VTLNumber.of((double) (number1 + number2) / (numbers.size() - 2)));
@@ -96,11 +96,20 @@ public class AggregationAvgFunctionTest {
         List<VTLNumber> numbers = Lists.newArrayList(
                 VTLNumber.of(number1),
                 VTLNumber.of(number2),
-                VTLNumber.of((Double)null),
+                VTLNumber.of((Double) null),
                 null
         );
         VTLNumber<?> result = new AggregationAvgFunction().apply(numbers);
         assertThat(result).isEqualTo(VTLNumber.of((number1 + number2) / (numbers.size() - 2)));
+    }
+
+    @Test
+    public void testReturnType() {
+        AggregationAvgFunction function = new AggregationAvgFunction();
+        assertThat(function.getVTLReturnTypeFor(Integer.class)).isEqualTo(Double.class);
+        assertThat(function.getVTLReturnTypeFor(Long.class)).isEqualTo(Double.class);
+        assertThat(function.getVTLReturnTypeFor(Double.class)).isEqualTo(Double.class);
+        assertThat(function.getVTLReturnTypeFor(Float.class)).isEqualTo(Double.class);
     }
 
 }
